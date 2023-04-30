f89d40902e1a26a906945572f21fef3f47a797f1
bayzhen <1521085156@qq.com>
2023-04-30 21:07:42 +0800

    项目启动

---
+++ Source/MarkovLibrary/MarkovLibraryGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MarkovLibraryGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMarkovLibraryGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMarkovLibraryGameMode();
+};
+
+
+
