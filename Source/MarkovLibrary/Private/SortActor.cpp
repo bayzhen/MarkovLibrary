39cb119ac0895f149a9ae5f7faf2997d68bf7859
bayzhen <1521085156@qq.com>
2023-08-19 22:40:47 +0800

    Update SortActor.cpp

--- Source/MarkovLibrary/Private/SortActor.cpp
+++ Source/MarkovLibrary/Private/SortActor.cpp
@@ -9,6 +9,7 @@ ASortActor::ASortActor()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	NumOfBases = 10;
 	InstanceDistance = 100.0;
 	MinimumHeight = 3;
 	MaximumHeight = 20;
@@ -17,21 +18,29 @@ ASortActor::ASortActor()
 	bGaming = true;
 	HISMComponent = CreateDefaultSubobject<UHierarchicalInstancedStaticMeshComponent>(TEXT("HISMComponent"));
 	this->SetRootComponent(Cast<USceneComponent>(HISMComponent));
-	// SortTable = TSharedPtr<FSortTable>(new FSortTable(
+}
+
+TArray<FTransform> ASortActor::GenerateBasesTransforms()
+{
+	TArray<FTransform> TransformArr;
+	TransformArr.Init(FTransform(), NumOfBases);
+	FVector HISMComponentLocation = HISMComponent->K2_GetComponentLocation();
+	for (int32 i = 0; i < NumOfBases; i++) {
+		FVector BaseLocation = HISMComponentLocation + FVector(0.0f, i * 150.0f, 0.0);
+		TransformArr[i].SetLocation(BaseLocation);
+	}
+	return TransformArr;
 }
 
 // Called when the game starts or when spawned
 void ASortActor::BeginPlay()
 {
 	Super::BeginPlay();
-	if (!bRandom) {
-		MyRandomStream.Initialize(Seed);
-	}
-	else {
-		MyRandomStream.Initialize(FMath::Rand());
-	}
-	HISMComponent->SetStaticMesh(StaticMesh);
-	//HISMComponent->OnComponentHit.Add
+	Reset();
+}
+
+void ASortActor::OnConstruction(const FTransform& Transform)
+{
 	Reset();
 }
 
@@ -40,13 +49,14 @@ void ASortActor::BeginPlay()
 void ASortActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	
+	SortTable->Tick(DeltaTime);
 }
 
 // 重置各各网格体的位置。
 void ASortActor::Reset()
 {
-
+	HISMComponent->SetStaticMesh(StaticMesh);
+	SortTable->GenerateBases(NumOfBases,
 }
 
 void ASortActor::GameStep(int32 PillarIndex)
